a332fca01474c4227e59949b1864ba2e060c6ab3
murattekbas <muratekbas@gmail.com>
2022-03-29 14:28:44 +0300

    AddAfter added

--- SinglyLinkedList/Program.cs
+++ SinglyLinkedList/Program.cs
@@ -6,6 +6,7 @@ linkedList.AddFirst(1);
 linkedList.AddFirst(2);
 linkedList.AddFirst(3);
 linkedList.AddLast(-5);
+linkedList.AddAfter(linkedList.Head.Next, 32);
 
 Console.ReadKey();
 
--- SinglyLinkedList/SinglyLinkedList.cs
+++ SinglyLinkedList/SinglyLinkedList.cs
@@ -9,6 +9,7 @@ namespace SinglyLinkedList
     public class SinglyLinkedList<T>
     {
         public SinglyLinkedListNode<T>? Head { get; set; }
+        private bool isHeadNull => Head == null;
 
         public void AddFirst(T value)
         {
@@ -21,7 +22,7 @@ namespace SinglyLinkedList
         {
             var newNode = new SinglyLinkedListNode<T>(value);
 
-            if (Head == null)
+            if (isHeadNull)
             {
                 Head = newNode;
                 return;
@@ -34,6 +35,28 @@ namespace SinglyLinkedList
             }
             
             if (current!=null) current.Next = newNode;
+        }
+
+        public void AddAfter(SinglyLinkedListNode<T> refNode,T value)
+        {
+            if (refNode == null) throw new ArgumentException("Reference node can not be null.");
+            if (isHeadNull) { 
+                AddFirst(value);
+                return;
+            }
+            var newNode = new SinglyLinkedListNode<T>(value);
+            var current = Head;
+            while (current!=null)
+            {
+                if (current.Equals(refNode))
+                {
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                    return;
+                }
+                current = current.Next;
+            }
+            throw new ArgumentException("The reference node is not in this list.");
         }
     }
 }
