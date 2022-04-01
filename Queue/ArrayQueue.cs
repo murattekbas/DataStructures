70bcfdfb18afa886ac79cd82876ae54f54b78253
murattekbas <muratekbas@gmail.com>
2022-04-01 16:43:04 +0300

    LinkedListQueue implemented

--- Queue/ArrayQueue.cs
+++ Queue/ArrayQueue.cs
@@ -4,22 +4,20 @@
     {
         private readonly List<T> list=new List<T>();   
         public int Count {get; private set;}    
-
         public T Dequeue()
         {
             if (Count == 0) throw new Exception("nothing to remove");
             var temp = list[0];
             list.RemoveAt(0);
+            Count--;
             return temp;
         }
-
         public void Enqueue(T value)
         {
            if (value == null) throw new ArgumentNullException("value");
            list.Add(value);
            Count++;
         }
-
         public T Peek()
         {
             if (Count == 0) throw new Exception("nothing to show");
--- Queue/LinkedListQueue.cs
+++ Queue/LinkedListQueue.cs
@@ -1,22 +1,31 @@
-﻿namespace Queue
+﻿using DoublyLinkedList;
+
+namespace Queue
 {
     public class LinkedListQueue<T> : IQueue<T>
     {
-        public int Count => throw new NotImplementedException();
+        private readonly DoublyLinkedList<T> list = new DoublyLinkedList<T>();
+        public int Count { get; private set; }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (Count == 0) throw new Exception("nothing to remove");
+            var temp=list.RemoveFirst();
+            Count--;
+            return temp;
         }
 
         public void Enqueue(T value)
         {
-            throw new NotImplementedException();
+            if (value == null) throw new ArgumentNullException("value");
+            list.AddLast(value);
+            Count++;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (Count == 0) throw new Exception("nothing to show");
+            return list.Head.Value;
         }
     }
 }
