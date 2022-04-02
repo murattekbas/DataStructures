ece0c556517a77c0ee0dd8f8f64d4bab94bc5fec
Murat Tekbas <murattekbas@outlook.com>
2022-04-02 18:49:11 +0300

    BST class added

---
+++ BinarySearchTree/BST.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BST<T> : IEnumerable<T> where T : IComparable
+    {
+        public Node<T>? Root { get;set; }
+        public BST()
+        {
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            var newNode=new Node<T>(value);
+
+            if(Root == null) 
+            {
+                Root = newNode;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
